347e2aed57410710918236a5111de0fa7fcf5546
Julian Laffey <julianlaffey@gmail.com>
2017-12-02 07:11:28 +1100

    Use arrow keys to select result

--- features/search/search.js
+++ features/search/search.js
@@ -4,7 +4,9 @@
 
 
 let state = {
-    searchPanelActive: false
+    searchPanelActive: false,
+    selectedResultIndex: 0,
+    triggerSelection: false
 };
 
 
@@ -13,11 +15,21 @@ const profiles = JSON.parse(localStorage.getItem('wikitreeturbo_watchlist'));
 
 function updateUI() {
 
+    // show/hide panel
     if (state.searchPanelActive) {
         $('#searchPanel').show();
         $('#searchPanel input').focus();
     } else {
         $('#searchPanel').hide();
+    }
+
+    // highlight a result
+    $('#searchPanel ul li').removeClass('selected');
+    $(`#searchPanel ul li:nth-child(${state.selectedResultIndex+1})`).addClass('selected');
+
+    // select result
+    if (state.triggerSelection) {
+        $(`#searchPanel ul li:nth-child(${state.selectedResultIndex+1}) a`)[0].click();
     }
 
 }
@@ -40,7 +52,23 @@ function isMatch(s, o) {
 }
 
 
+function createSearchPanel() {
+    let div = $(`
+    <div id="searchPanel">
+        <input type="text" placeholder="Start typing..." />
+        <ul></ul>
+        <p>ARROW UP/DOWN to select<br>
+        ENTER to open<br>
+        BACKQUOTE (\`) to close</p>
+    </div>
+`);
+    $('body').append(div);
+}
+
+
 function onInputChange(e) {
+
+    state.selectedResultIndex = 0;    
 
     if (profiles == null) {
         alert('No watchlist data found. Please visit your watchlist page, then try again.');
@@ -53,33 +81,43 @@ function onInputChange(e) {
 }
 
 
-function createSearchPanel() {
-    let div = $(`
-    <div id="searchPanel">
-        <input type="text" placeholder="Start typing..." />
-        <ul></ul>
-        <p>TAB - next<br>
-        SHIFT+TAB - back<br>
-        ENTER - select<br>
-        BACKQUOTE (\`) - close</p>
-    </div>
-`);
-    $('body').append(div);
-}
-
-
-function onKeyPress(e) {
-
+// separate fn to supress keypress
+function onBackquote(e) {
     if (e.originalEvent.code == 'Backquote') {
         state.searchPanelActive = !state.searchPanelActive;
         updateUI();
         e.preventDefault();
+    }  
+}
+
+
+function onArrowOrEnter(e) {
+
+    code = e.originalEvent.code;
+
+    if (code == 'ArrowDown') {
+        state.selectedResultIndex++;
     }
 
+    else if (code == 'ArrowUp') {
+        state.selectedResultIndex--;
+    }
+
+    else if (code == 'Enter') {
+        state.triggerSelection = true;
+    }
+
+    updateUI();    
+}
+
+
+function onEnter(e) {
+    console.log('enter');
 }
 
 
 createSearchPanel();
 
 $('#searchPanel input').on('input', onInputChange);
-$('body').keypress(onKeyPress);
+$('body').keypress(onBackquote);
+$('#searchPanel').keyup(onArrowOrEnter);
