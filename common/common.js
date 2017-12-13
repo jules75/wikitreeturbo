281328d0c78c0318b91f34976d3ffb13a39e5222
Julian Laffey <julianlaffey@gmail.com>
2017-12-14 08:34:03 +1100

    Use WikiTree's own style for toasts

--- common/common.js
+++ common/common.js
@@ -1,7 +1,14 @@
 
 // display a 'toast' (fading message) in top right corner
 function toast(msg) {
-    const el = $(`<p id="toast">${msg}</p>`);
+    const el = $(`
+    <div id="toast-container" class="toast-top-right">
+        <div class="toast toast-success" aria-live="polite" style="display: block;">
+            <div class="toast-message">${msg}</div>
+        </div>
+    </div>
+    `);
     $('body').prepend(el);
-    $(el).fadeOut(4000);
+    $(el).delay(4000).fadeOut(1000);
 }
+
