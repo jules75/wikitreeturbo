0cb4a46fcb601329f24f93e548d5137fe4618038
Julian Laffey <julianlaffey@gmail.com>
2017-12-13 14:03:09 +1100

    Refactor: consolidate js into single file

--- features/search/watchlist.js
+++ features/search/watchlist.js
@@ -3,6 +3,25 @@
  */
 
 
+// reducing fn
+function build(result, value, key) {
+
+    const td1 = $(value).children().eq(0);
+    const link = $(td1).children('a')[0];
+
+    const td2 = $(value).children().eq(1)[0];
+    const profile = link.href.match(/wiki\/(.*)$/)[1];
+
+    result[profile] = {
+        'name': link.innerText,
+        'url': link.href,
+        'born': td2.innerText
+    };
+
+    return result;
+}
+
+
 const key = 'wikitreeturbo_watchlist';
 const rows = _.drop($('table tr'), 1);
 const s = localStorage.getItem(key);
