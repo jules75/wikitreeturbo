ca3a32b45dd150af6e9aa1db53a670fbd35768db
Julian Laffey <julianlaffey@gmail.com>
2017-12-13 14:00:37 +1100

    Add any visited profile to quick search index

---
+++ features/search/profile.js
@@ -0,0 +1,25 @@
+/*
+ * When on profile page, scrape single profile data and save to localstorage
+ */
+
+
+const key = 'wikitreeturbo_watchlist';
+const s = localStorage.getItem(key);
+const profile = location.href.match(/wiki\/(.*)$/)[1]
+const name = $('h1 span[itemprop="name"]').text();
+
+let profs = JSON.parse(s) || {};
+
+const profileAlreadyExists = Boolean(profs[profile]);
+
+profs[profile] = {
+    'name': name,
+    'url': location.href,
+    'born': $('time[itemprop="birthDate"]').text()
+};
+
+localStorage.setItem(key, JSON.stringify(profs));
+
+if (!profileAlreadyExists) {
+    toast(name + " added to quick search index");
+}
